22b50864d70dd3957f436c744dfd890b0476c30b
Betty Baker <betty@heysparkbox.com>
2017-02-24 17:04:08 -0500

    first commit

---
+++ git_muscle_memory.rb
@@ -0,0 +1,9 @@
+# This exercise is designed to help build muscle memory when using
+# git and gitHub from the command line.
+
+# Add a new character, save changes, commit. Easy. Do it a lot. Then some more.
+
+# Recommend binge watching Santa Clarita Diet while doing this exercise,
+# because zombies.
+
+first commit!
